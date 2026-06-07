65e7baf1c49b9cfb6922dc86e8c6bedc8bf408a2
sourlmn <avocadoalien030@gmail.com>
2026-06-07 07:52:50 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -167,7 +167,7 @@ export const botConfig = {
     startingBalance: 50,
 
     // Maximum bank amount before upgrades (if upgrades are used).
-    baseBankCapacity: 500000,
+    baseBankCapacity: 500000000,
 
     // Daily reward amount.
     dailyAmount: 20,
